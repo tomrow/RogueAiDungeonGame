c681f01008fdeb48f6db73405ed461dddc4c668b
tomrow <tomrow.bonzi@gmail.com>
2026-02-20 19:55:04 +0000

    Update mainMenuCtl.cs

--- Assets/mainMenuCtl.cs
+++ Assets/mainMenuCtl.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class mainMenuCtl : MonoBehaviour
 {
+    public List<GameObject> menuItems;
     Material material; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
