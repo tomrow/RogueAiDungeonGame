c15cd6604cda26cf7dfcb0bf92296e0bf946e814
Rowan Sangster <Rowan.j.s123@live.co.uk>
2026-04-22 14:52:20 +0100

    Literally Just A Gate!
    That's ALL THAT CHANGED!
    
    WOO!
    
    GATES!
    
    YEAH!
    
    Sarcasm aside. There's now a rough gate object. and a switch that activates it.
    Some things were broken on main, so they're in-progress of being fixed here.
    Navmesh has been re-baked. Again.

---
+++ Assets/Row's Crappy Prototyping assets/Scripts/GateSwitch.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class GateSwitch : MonoBehaviour
+{
+    [SerializeField, Tooltip("This is the switch's accompanying gate.")] GameObject MyDoor;
+    LaserDoor laserDoorReference;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!MyDoor)
+        {
+            Debug.Log("There's no gate attached to" + this.gameObject + "Please return to editor and attach a gameobject.");
+        }
+        //At this point, this might be closer to an INsanity check.
+        // well, anyway... This just adds another log to the alrady bloated console.
+        // sorry tom...
+        // = row.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        laserDoorReference = MyDoor.GetComponentInChildren<LaserDoor>();
+        Debug.Log(laserDoorReference);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerCtl>()) //So. If the player triggers the object...
+        {
+            laserDoorReference = MyDoor.GetComponentInChildren<LaserDoor>(); // pull from the connected object's children to find the LaserDoor script.
+            laserDoorReference.GateHasBeenOpened = true; //open the door.
+        }
+    }
+}
---
+++ Assets/Row's Crappy Prototyping assets/Scripts/LaserDoor.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class LaserDoor : MonoBehaviour
+{
+    [SerializeField, Tooltip("This is the object that needs to remain for this one to keep doing it's usual behaviour.")] GameObject PowerGenerator;
+    [SerializeField, Tooltip("This is the timer for timed gates. This defines the amount of time the gate remains open.")] float TimeOpenedMax;
+    [SerializeField, Tooltip("This Gate is timed.")] public bool GateOpeningIsTimed;
+    public bool GateHasBeenOpened;
+    #region ARGH
+    GameObject SHITEASS1;
+    GameObject SHITEASS2;
+    GameObject SHITEASS3;
+    #endregion
+    float timeRemainingOpen;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!PowerGenerator)
+        {
+            Debug.Log("Object is Unpowered" + this.gameObject + "Will now be deleted.");
+            Destroy(this.gameObject);
+        }
+        timeRemainingOpen = TimeOpenedMax;
+    }
+    // Just a real simple sanity check. If there's no generator assigned, the gate auto-opens.
+
+    // Update is called once per frame
+    void Update()
+    {
+        SHITEASS1 =this.gameObject.transform.Find("Shitass1").gameObject;
+        SHITEASS2 = this.gameObject.transform.Find("shitass2").gameObject;
+        SHITEASS3 = this.gameObject.transform.Find("shitassCollision").gameObject;// uhh? Take child objects from this script's container?
+        if (PowerGenerator) //if there IS a generator...
+        {
+            if (GateHasBeenOpened == false)
+            {
+                SHITEASS1.SetActive(true); // The gate closes.
+                SHITEASS2.SetActive(true); // The gate closes.
+                SHITEASS3.SetActive(true); // The gate closes.
+            }
+            else if (GateHasBeenOpened && GateOpeningIsTimed) // else, if the gate is open and the gate has a timer...
+            {
+                SHITEASS1.SetActive(false); // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                SHITEASS2.SetActive(false); // WHO'S IDEA WAS "this.gameObject.transform.Find("shitassCollision").gameObject;", ESPECIALLY IN A LANGUAGE THAT EMPHASISES PUTTING THINGS, ATTHEFRONT!
+                SHITEASS3.SetActive(false); //set selected objects to Inactive?
+                timeRemainingOpen -= Time.deltaTime; // start decrementing the timer.
+
+                if (timeRemainingOpen <= 0)// if timer runs out...
+                {
+                    timeRemainingOpen = TimeOpenedMax; //reset timer.
+                    GateHasBeenOpened = false; // close gate.
+                }
+            }
+            else if(GateHasBeenOpened && !GateOpeningIsTimed) // else, if the gate is opened and not timed then...
+            {
+                Destroy(this.gameObject); // destroy the gate.
+            }
+
+        }
+    }
+}
+#region ARGH2
+// THERE  BETTER BE NO MORE FUCKING PROBLEMS, OR ELSE I'M GONNA FUCKING LOSE IT!
+// UNDER NO CIRCUMSTANCES WOULD ANY NORMAL PERSON EVER BE EXPECTED TO KNOW THAT
+//     "this.gameObject.transform.Find("shitassCollision").gameObject;"
+// WORKS LIKE >THAT<!
+// NOBODY IS! SERIOUSLY, WHO DECIDED THAT!? LEMME AT 'EM!
+#endregion
