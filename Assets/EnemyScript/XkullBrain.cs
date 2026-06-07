29c7e366e8f6350145e35bb193b7a91187f9790b
tomrow <tomrow.bonzi@gmail.com>
2026-06-07 20:32:53 +0100

    extra collision for xkull, revert from vulkan to dx11 to combat animation glitches
    In the hubworld, the character would randomly get scrambled bones for one frame at random intervals

--- Assets/EnemyScript/XkullBrain.cs
+++ Assets/EnemyScript/XkullBrain.cs
@@ -6,6 +6,7 @@ public class XkullBrain : MonoBehaviour
     public Vector3 translatePerSecond;
     public float followPlayerSpeed;
     Vector3 dirToPlayer;
+    RaycastHit rayout;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,9 +16,27 @@ public class XkullBrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(translatePerSecond*Time.deltaTime);
+        
         transform.Rotate(rotateDegreesPerSecond*Time.deltaTime);
         dirToPlayer = (PlayerHealth.thisPlayer.transform.position - transform.position).normalized ;
+        
+        Vector3 global =
+            transform.right * translatePerSecond.x +
+            transform.up * translatePerSecond.y +
+            transform.forward * translatePerSecond.z;
+        if (Physics.Raycast(transform.position, 
+            ((dirToPlayer * followPlayerSpeed * Time.deltaTime) + (global * Time.deltaTime)).normalized, 
+            out rayout, 
+            ((dirToPlayer * followPlayerSpeed * Time.deltaTime) + (global * Time.deltaTime)).magnitude))
+        { 
+            PlayerCtl p = rayout.collider.gameObject.GetComponent<PlayerCtl>();
+            try
+            { p.DamageFrom(transform, 6, 1); }
+            catch { }
+            transform.Rotate(0, 180, 0);
+            transform.Translate(translatePerSecond * Time.deltaTime);
+        }
+        transform.Translate(translatePerSecond*Time.deltaTime);
         transform.position += dirToPlayer * followPlayerSpeed * Time.deltaTime;
     }
     private void OnTriggerEnter(Collider other)
